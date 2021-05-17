977a9975381b6d61381f42a81ec6e772ea4618f3
sina <sinaali.7.70@gmail.com>
2021-05-17 15:05:53 -0700

    first push

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Nav, NavLink} from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'> Pizaa</NavLink>    
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
